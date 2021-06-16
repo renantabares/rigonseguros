c96c0aaa439474600510d3686ee0003886a35ff3
igor-ifrs <icc@pop-os.localdomain>
2021-06-16 13:51:32 -0300

    inclusao lint e prettierrc :package:

--- src/main/App.js
+++ src/main/App.js
@@ -4,11 +4,10 @@ import Route from './routes'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
 
-export default props => (
-    <div className="App" >
+export default (props) => (
+    <div className="App">
         <Menu></Menu>
         <Route></Route>
         <Footer></Footer>
     </div>
-
 )
--- src/seguradoras/index.jsx
+++ src/seguradoras/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
-
-export default props => {
-
+export default (props) => {
     return (
-        <div className='container'>
+        <div className="container">
             <Breadcrumb className="dark">
                 <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
                 <Breadcrumb.Item href="#/seguradoras">
                     Seguradoras
-        </Breadcrumb.Item>
+                </Breadcrumb.Item>
             </Breadcrumb>
             <h2>Seguradoras</h2>
         </div>
